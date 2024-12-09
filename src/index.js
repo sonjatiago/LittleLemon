12f48270d2ad795a4a2e251ed9bff76cc66c4a14
sonjatiago <sonjatiago@outlook.com>
2024-12-09 15:42:47 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import './index.css'; // Optional, add your global styles here
+import App from './App';
+
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
